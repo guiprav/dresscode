6c86f90a6a8598e8029ff3136376492c5978977e
Guilherme Vieira <super.driver.512@gmail.com>
2015-08-22 01:06:24 -0300

    Implement static file serving.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let express = require('express');
 let routes = require('./routes');
 let app = express();
 let port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/static'));
 routes.register(app);
 app.listen(port);
 console.log("Listening on port", port + ".");
