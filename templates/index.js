6e7dbe3a890864aa22de9dbc5576f6d2f6e80c13
Guilherme Vieira <super.driver.512@gmail.com>
2015-08-22 01:45:14 -0300

    Implement basis for template partials.

--- templates/index.js
+++ templates/index.js
@@ -3,6 +3,10 @@ let baseName = require('path').basename;
 let _ = require('lodash');
 let hbs = require('handlebars');
 let readGlobSync = require('../readGlobSync');
+_.each(readGlobSync(__dirname + '/partials/*.hbs'), function(sourceCode, path) {
+    let name = baseName(path, '.hbs');
+    hbs.registerPartial(name, sourceCode);
+});
 _.each(readGlobSync(__dirname + '/*.hbs'), function(sourceCode, path) {
     let name = baseName(path, '.hbs');
     exports[name] = hbs.compile(sourceCode);
